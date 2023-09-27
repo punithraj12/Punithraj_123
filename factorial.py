142fa6a9a0ef394a37602e7f1ba51a3bee7dd79b
punithraj12 <146169895+punithraj12@users.noreply.github.com>
2023-09-27 12:53:58 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,16 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+# Input from the user
+try:
+    num = int(input("Enter a number: "))
+    if num < 0:
+        print("Factorial is not defined for negative numbers.")
+    else:
+        result = factorial(num)
+        print(f"The factorial of {num} is {result}")
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
